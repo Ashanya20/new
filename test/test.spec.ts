59c84285daa243d17a1b1f38d3f9b3fd4f1fd2c1
SamJulia <54730475+SamJulia@users.noreply.github.com>
2021-04-21 09:46:11 -0700

    Add files via upload

---
+++ test/test.spec.ts
@@ -0,0 +1,13 @@
+//import assert from 'assert';
+//let WindowsDriver = require('appium-windows-driver').WindowsDriver;
+
+describe('Samples', () => {
+	/*beforeEach(() => {
+		
+	});*/
+
+	it('ButtonPage', () => {
+		console.log(`-----${window}`);
+		console.log(`====${Window.name}`);
+	});
+});
